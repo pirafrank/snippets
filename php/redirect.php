6e6f353709d389a657c6f3de1d4621ad27dd7638
Francesco Pira <dev@fpira.com>
2016-04-10 20:41:42 +0200

    added php and html redirects

---
+++ php/redirect.php
@@ -0,0 +1,9 @@
+<!--
+edit url to customise it
+then rename this file to index.php
+and place it in proper folder on website
+-->
+
+<?php
+header( 'Location: http://www.yoursite.com/redirect_location' );
+?>
