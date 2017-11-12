cb94c3f6b53703a6b0514bb574e4abbbef996530
Francesco Pira <dev@fpira.com>
2017-11-12 16:21:43 +0100

    symlinking to /usr instead of /usr/local

--- node/installer_nodejs.sh
+++ node/installer_nodejs.sh
@@ -28,19 +28,33 @@ mv node-$VERSION-linux-armv6l/* /opt/nodejs/$VERSION/
 rm -rf /tmp/nodejs
 
 # Removing symlinks to existing installation
-rm /usr/local/bin/node
-rm /usr/local/bin/npm
-rm /usr/local/include/node
-rm /usr/local/lib/node_modules
-rm /usr/local/share/doc/node
-rm /usr/local/share/man/man1/node.1
-rm /usr/local/share/systemtap/tapset/node.stp
+#rm /usr/local/bin/node
+#rm /usr/local/bin/npm
+#rm /usr/local/include/node
+#rm /usr/local/lib/node_modules
+#rm /usr/local/share/doc/node
+#rm /usr/local/share/man/man1/node.1
+#rm /usr/local/share/systemtap/tapset/node.stp
+rm /usr/bin/node
+rm /usr/bin/npm
+rm /usr/include/node
+rm /usr/lib/node_modules
+rm /usr/share/doc/node
+rm /usr/share/man/man1/node.1
+rm /usr/share/systemtap/tapset/node.stp
 
 # Create symlinks to node and npm
-ln -s /opt/nodejs/$VERSION/bin/node /usr/local/bin/node
-ln -s /opt/nodejs/$VERSION/bin/npm /usr/local/bin/npm
-ln -s /opt/nodejs/$VERSION/include/node /usr/local/include/node
-ln -s /opt/nodejs/$VERSION/lib/node_modules /usr/local/lib/node_modules
-ln -s /opt/nodejs/$VERSION/share/doc/node /usr/local/share/doc/node
-ln -s /opt/nodejs/$VERSION/share/man/man1/node.1 /usr/local/share/man/man1/node.1
-ln -s /opt/nodejs/$VERSION/share/systemtap/tapset/node.stp /usr/local/share/systemtap/tapset/node.stp
+#ln -s /opt/nodejs/$VERSION/bin/node /usr/local/bin/node
+#ln -s /opt/nodejs/$VERSION/bin/npm /usr/local/bin/npm
+#ln -s /opt/nodejs/$VERSION/include/node /usr/local/include/node
+#ln -s /opt/nodejs/$VERSION/lib/node_modules /usr/local/lib/node_modules
+#ln -s /opt/nodejs/$VERSION/share/doc/node /usr/local/share/doc/node
+#ln -s /opt/nodejs/$VERSION/share/man/man1/node.1 /usr/local/share/man/man1/node.1
+#ln -s /opt/nodejs/$VERSION/share/systemtap/tapset/node.stp /usr/local/share/systemtap/tapset/node.stp
+ln -s /opt/nodejs/$VERSION/bin/node /usr/bin/node
+ln -s /opt/nodejs/$VERSION/bin/npm /usr/bin/npm
+ln -s /opt/nodejs/$VERSION/include/node /usr/include/node
+ln -s /opt/nodejs/$VERSION/lib/node_modules /usr/lib/node_modules
+ln -s /opt/nodejs/$VERSION/share/doc/node /usr/share/doc/node
+ln -s /opt/nodejs/$VERSION/share/man/man1/node.1 /usr/share/man/man1/node.1
+ln -s /opt/nodejs/$VERSION/share/systemtap/tapset/node.stp /usr/share/systemtap/tapset/node.stp
